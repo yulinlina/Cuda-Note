b80d86e950374d2f87110c6ee203a814b53d0b2e
yulinlin <77262518+yulinlina@users.noreply.github.com>
2022-06-25 09:51:45 +0800

    Add files via upload

---
+++ matrix_transpose/error_check.h
@@ -0,0 +1,12 @@
+#ifndef ERROR_CHECK_H
+#define ERROR_CHECK_H
+#define CHECK(call){                                                    \
+    cudaError_t e_code = call;                                          \
+        if(e_code!=cudaSuccess){                                        \
+            printf("##CUDA Error:\n");                                  \
+            printf("  File: %s\n", __FILE__);                           \
+            printf("  Line: %d\n", __LINE__);                           \
+            printf("  Error info: %s\n", cudaGetErrorString(e_code));   \
+        }                                                               \
+}
+#endif
